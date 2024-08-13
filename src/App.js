ef8cc7e06d854c87fb49468d4cf1c27796e9efac
hyoseonlim <hyoseonee0729@naver.com>
2024-08-13 20:20:32 +0900

    관리자 기본 구조 변경

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { Routes,Route, BrowserRouter } from 'react-router-dom';
+import AdminHome from './Admin/AdminHome';
+import AdminMain from './Admin/AdminMain/AdminMain';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+    <BrowserRouter>
+     <Routes>
+       <Route path="/admin" element={<AdminHome />} />
+       <Route path="/admin/main" element={<AdminMain />} />
+      </Routes>
+    </BrowserRouter> 
+    </div> 
+  );
 }
 
 export default App;
