f5b6e7b595b8366c2534084f74a3e11f55674db1
dazz6 <dazz6@kakao.com>
2024-08-14 19:19:32 +0900

    main by dazz6

--- src/User/Main/Main.js
+++ src/User/Main/Main.js
@@ -1,6 +1,25 @@
 import React, { useState } from "react";
 
 const Main = () => {
+  const [members, setMembers] = useState([]);
+  const refresh = () => {
+    // Ajax 요청 (Get 방식) 설정
+    axios
+      .get("/members")
+      .then((res) => {
+        // 서버로부터 응담된 데이터를 이용해 state 수정
+        setMembers(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const showData = () => {};
+  const showNewReview = () => {};
+  const showStyleBest = () => {};
+  useEffect(() => {
+    refresh(); // Ajax 요청 처리
+  }, []);
   return (
     <div>
       <img width="100%" src="images/mainphoto-01.png" alt="main"></img>
