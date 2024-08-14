2943306dfce60313a8fb924d2b1ab3c1ef656ea8
dazz6 <dazz6@kakao.com>
2024-08-14 19:11:16 +0900

    userroutes 수정

--- src/User/Main/Main.js
+++ src/User/Main/Main.js
@@ -3,7 +3,11 @@ import React, { useState } from "react";
 const Main = () => {
   return (
     <div>
+      <img width="100%" src="images/mainphoto-01.png" alt="main"></img>
       <b>SceneStealer</b>
+      <div className="mainTextTitle">Choose Your Scene!</div>
+      <div className="mainTextTitle">New Review</div>
+      <div className="mainTextTitle">Style Best</div>
     </div>
   );
 };
--- src/User/Routes/UserRoutes.js
+++ src/User/Routes/UserRoutes.js
@@ -3,10 +3,12 @@ import UserMainRoutes from "./UserMainRoutes";
 import UserMypageRoutes from "./UserMypageRoutes";
 import UserShopRoutes from "./UserShopRoutes";
 import UserStyleRoutes from "./UserStyleRoutes";
+import Main from "../Main/Main";
 
 export default function UserRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Main />} />
       <Route path="/shop/*" element={<UserShopRoutes />} />
       <Route path="/style/*" element={<UserStyleRoutes />} />
       <Route path="/mypage/*" element={<UserMypageRoutes />} />
