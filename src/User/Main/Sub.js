2a7dc352e88849d2427d48736def4bc5cabe788b
dazz6 <dazz6@kakao.com>
2024-08-15 17:32:44 +0900

    subpage

--- src/User/Main/Sub.js
+++ src/User/Main/Sub.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Sub() {
   const { no } = useParams();
+  const [character, setCharacter] = useState([]);
+  const [style, setStyle] = useState([]);
+  const [item, setItem] = useState([]);
+  const [scrap, setScrap] = useState([]);
 
   return (
-    <div>
-      어쩌구<div>어쩌구!!{no}</div>
-    </div>
+    <>
+      <div></div>
+    </>
   );
 }
