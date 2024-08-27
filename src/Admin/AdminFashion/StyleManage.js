5a4d101e79f50bcd1baebdd1cab696c12468dad0
hyo <hyoseonee0729@naver.com>
2024-08-28 01:48:57 +0900

    관리자 메인

--- src/Admin/AdminFashion/StyleManage.js
+++ src/Admin/AdminFashion/StyleManage.js
@@ -2,22 +2,34 @@ import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import Modal from "react-modal";
+import styled from "styled-components";
 
 export default function StyleManage() {
   const { no } = useParams();
-  const [styles, setStyles] = useState([]);
-  const [newStyle, setNewStyle] = useState(null);
-  const [items, setItems] = useState([]);
-  const [newItem, setNewItem] = useState(null);
-  const [productKeyword, setProductKeyword] = useState('');
-  const [itemKeyword, setItemKeyword] = useState('');
-  const [itemInput, setItemName] = useState('');
   const location = useLocation();
   const actorData = location.state;
   const navigate = useNavigate();
+
+  const [styles, setStyles] = useState([]);
+  const [newStyle, setNewStyle] = useState(null);
   const styleInputRef = useRef(null);
+
+  const [items, setItems] = useState([]);
+  const [newItem, setNewItem] = useState(null);
+  const [productKeyword, setProductKeyword] = useState(''); // 상품 검색 입력값
+  const [itemKeyword, setItemKeyword] = useState(''); // 아이템 검색 입력값
+  const [newItemName, setNewItemName] = useState(''); // 새로운 아이템 이름
+
+  const [itemNo, setItemNo] = useState();
+  
   const [isNewItemModalOpen, setIsNewItemModalOpen] = useState(false);
   const [isExistingItemModalOpen, setIsExistingItemModalOpen] = useState(false);
+
+  const [showItemDropdown, setShowItemDropdown] = useState(false);
+  const [showProductDropdown, setShowProductDropdown] = useState(false);
+
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [currentStyle, setCurrentStyle] = useState({});
 
   useEffect(() => {
@@ -53,14 +65,58 @@ export default function StyleManage() {
 
   const onProductKeywordChange = (e) => {
     setProductKeyword(e.target.value);
+    fetchProductData();
   }
 
   const onItemKeywordChange = (e) => {
     setItemKeyword(e.target.value);
+    fetchItemData();
   }
 
+  const fetchItemData = async () => {
+    if (itemKeyword) {
+      try {
+        const response = await axios.get(`/admin/item/autocomplete/${itemKeyword}`);
+        setFilteredItems(response.data);
+      } catch (error) {
+        console.error(`너가 입력한 ${itemKeyword}에서 못 가져오겠어: `, error);
+        setFilteredItems([]);
+      }
+    } else {
+      // 입력값 없을 때는 전체 목록을 가져온다.
+      try {
+        const response = await axios.get('/admin/item/autocomplete');
+        setFilteredItems(response.data);
+      } catch (error) {
+        console.error("아이템 못 가져오겠어: ", error);
+        setFilteredItems([]);
+      }
+    }
+  };
+
+  const fetchProductData = async () => {
+    if (productKeyword) {
+      try {
+        const response = await axios.get(`/admin/product/autocomplete/${productKeyword}`);
+        setFilteredProducts(response.data);
+      } catch (error) {
+        console.error(`너가 입력한 ${itemKeyword}에서 못 가져오겠어: `, error);
+        setFilteredProducts([]);
+      }
+    } else {
+      // 입력값 없을 때는 전체 목록을 가져온다.
+      try {
+        const response = await axios.get('/admin/product/autocomplete');
+        setFilteredProducts(response.data);
+      } catch (error) {
+        console.error("상품 못 가져오겠어: ", error);
+        setFilteredProducts([]);
+      }
+    }
+  };
+
   const onItemNameChange = (e) => {
-    setItemName(e.target.value);
+    setNewItemName(e.target.value);
   }
 
   const addStyle = async() => {
@@ -83,13 +139,31 @@ export default function StyleManage() {
       });
   }
 
-  const addItem = async(styleNo) => {
+  const addExistingItem = async(style_no, item_no) => {
+    setIsExistingItemModalOpen(false);
+    await axios
+      .post(`/admin/fashion/${style_no}/item/${item_no}`)
+      .then(
+        axios
+        .get(`/admin/fashion/character/${no}/item`)
+        .then((response) => {
+          setItems(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        }))
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  const addItem = async(style_no) => {
     const itemForm = new FormData();
     itemForm.append('file', newItem);
     itemForm.append('product', productKeyword);
     
     await axios
-      .post(`/admin/fashion/${styleNo}/item`, itemForm, {
+      .post(`/admin/fashion/${style_no}/item`, itemForm, {
         headers: {'Content-Type': 'multipart/form-data'}
       })
       .then((response) => {
@@ -123,26 +197,25 @@ export default function StyleManage() {
           </tr>
         </thead>
         <tbody>
-          {styles.map((styleData, index) => {
-            // styleData.no와 일치하는 items 필터링
+          {styles.map((styleData, index) => { // styleData.no와 일치하는 items 필터링
             const filteredItems = items.filter(item => item.style === styleData.no);
-
             return (
               <tr key={index}>
                 <td><img src={styleData.pic} alt={`${index + 1}번 스타일`} /></td>
                 {[0, 1, 2].map((i) => (
                   <td key={i}>
                     {filteredItems[i] ? 
-                      (<><img src={filteredItems[i].pic} alt={`${index}번 스타일 아이템${i+1}`} />
-                      <br/><button onClick={() => navigate(`/admin/product/detail/${filteredItems[i].product}`)}>유사상품 조회하기</button></>) 
+                      (<>{filteredItems[i].name}<br/>
+                      <img src={filteredItems[i].pic} alt={`${index}번 스타일 아이템${i+1}`} /><br/>
+                      <button onClick={() => navigate(`/admin/product/detail/${filteredItems[i].product}`)}>유사상품 조회하기</button></>) 
                     : (<>
                           <button onClick={() => {
-                            setCurrentStyle(styleData);  // Set the current style number
+                            setCurrentStyle(styleData);
                             setIsNewItemModalOpen(true);
                           }}>🛍️새로운 아이템으로 추가하기🛍️</button>
                           <br/><br/>
                           <button onClick={() => {
-                            setCurrentStyle(styleData);  // Set the current style number
+                            setCurrentStyle(styleData);
                             setIsExistingItemModalOpen(true);
                           }}>🛍️기존 아이템으로 추가하기🛍️</button>
                       </>)}
@@ -153,26 +226,33 @@ export default function StyleManage() {
           })}
         </tbody>
       </table>
+      
       <Modal
         isOpen={isNewItemModalOpen}
         onRequestClose={() => setIsNewItemModalOpen(false)}
         contentLabel="새로운 아이템 추가하기"
-        style={{
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          },
-          content: {
-            background: "white",
-            padding: "20px",
-            borderRadius: "8px",
-            textAlign: "center",
-            maxWidth: "500px",
-            margin: "auto",
-          },
-        }}
+        style={{overlay: {backgroundColor: "rgba(0, 0, 0, 0.5)",},
+                content: {background: "white",padding: "20px", borderRadius: "8px", textAlign: "center", maxWidth: "500px", margin: "auto",},}}
       >
         <img height='100px' src={currentStyle.pic} alt={`${currentStyle.no}번 스타일`} /><br/>
-        {currentStyle.no}번 스타일에 신규 아이템 연결하기<hr/>
+        {currentStyle.no}번 스타일에 신규 아이템 연결하기
+        <button onClick={() => setIsNewItemModalOpen(false)}>닫기</button>
+        
+        <SearchForm>
+          <SearchInput type="text" placeholder="연결할 상품명을 입력하세요" onChange={onProductKeywordChange} 
+          onFocus={() => {setShowProductDropdown(true); fetchProductData()}} 
+          />
+          {showProductDropdown && (
+          <AutoSearchContainer>
+          {filteredProducts.map((product, index) => (
+            <AutoSearchItem key={index}>
+              {product.name} <img height='100px' src={product.pic} alt={`${product.name} 사진`} />
+              <SearchButton>연결</SearchButton>
+            </AutoSearchItem>
+          ))}
+          </AutoSearchContainer>
+          )}
+        </SearchForm>
         <table>
           <tbody>
             <tr>
@@ -183,39 +263,114 @@ export default function StyleManage() {
               <td>아이템 이름</td>
               <td><input type="text" onChange={onItemNameChange} /></td>
             </tr>
-            <tr>
-              <td>연결 상품 검색</td>
-              <td><input type="text" onChange={onProductKeywordChange} /></td>
-            </tr>
           </tbody>
         </table>
-        <button onClick={() => addItem}>추가</button><hr/>
-        <button onClick={() => setIsNewItemModalOpen(false)}>닫기</button>
+        <button onClick={() => addItem}>추가</button>
       </Modal>
 
       <Modal
         isOpen={isExistingItemModalOpen}
         onRequestClose={() => setIsExistingItemModalOpen(false)}
         contentLabel="기존 아이템 추가하기"
-        style={{
-          overlay: {
-            backgroundColor: "rgba(0, 0, 0, 0.5)",
-          },
-          content: {
-            background: "white",
-            padding: "20px",
-            borderRadius: "8px",
-            textAlign: "center",
-            maxWidth: "500px",
-            margin: "auto",
-          },
-        }}
+        style={{overlay: {backgroundColor: "rgba(0, 0, 0, 0.5)",},
+                content: {background: "white",padding: "20px", borderRadius: "8px", textAlign: "center", maxWidth: "500px", margin: "auto",},}}
       >
+        {currentStyle.no}번 스타일에 기존 아이템 연결하기<button onClick={() => setIsExistingItemModalOpen(false)}>닫기</button><br/><br/>
         <img height='100px' src={currentStyle.pic} alt={`${currentStyle.no}번 스타일`} /><br/>
-        {currentStyle.no}번 스타일에 기존 아이템 연결하기<hr/>
-        아이템 검색 <input type="text" onChange={onItemKeywordChange} /><br/>
-        <button onClick={() => setIsExistingItemModalOpen(false)}>닫기</button>
+        <br/>
+        <SearchForm>
+          <SearchInput type="text" placeholder="아이템 이름을 입력하세요" onChange={onItemKeywordChange} 
+          onFocus={() => {setShowItemDropdown(true); fetchItemData();}} 
+          />
+          {showItemDropdown && (
+          <AutoSearchContainer>
+          {filteredItems.map((item, index) => (
+            <AutoSearchItem key={index}>
+              {item.name} <img height='100px' src={item.pic} alt={`${item.name} 사진`} />
+              <SearchButton onMouseDown={() => {addExistingItem(currentStyle.no, item.no); setIsExistingItemModalOpen(false);}}>연결하여 추가</SearchButton>
+            </AutoSearchItem>
+          ))}
+          </AutoSearchContainer>
+        )}
+        </SearchForm>
       </Modal>
     </>
   );
 }
+
+const SearchForm = styled.form`
+  display: flex;
+  align-items: center;
+  position: relative; /* 드롭다운의 위치를 제대로 설정하기 위해 추가 */
+`;
+
+const SearchInput = styled.input`
+  font-family: inherit;
+  font-size: inherit;
+  background-color: white;
+  border: none;
+  color: #646464;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  width: 12em;
+  transition: all ease-in-out 0.5s;
+  margin-right: 0.5rem;
+
+  &:hover,
+  &:focus {
+    box-shadow: 0 0 1em #00000013;
+  }
+
+  &:focus {
+    outline: none;
+    background-color: #f0eeee;
+  }
+
+  &::-webkit-input-placeholder {
+    font-weight: 100;
+    color: #ccc;
+  }
+`;
+
+const SearchButton = styled.button`
+  font-family: inherit;
+  font-size: inherit;
+  background-color: #323232;
+  color: #fff;
+  border: none;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #505050;
+    cursor: pointer;
+  }
+`;
+
+const AutoSearchContainer = styled.div`
+  position: absolute;
+  top: 45px;
+  left: 0;
+  width: 400px;
+  max-height: 200px;
+  overflow-y: auto;
+  background-color: #fff;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  box-shadow: 0 10px 10px rgb(0, 0, 0, 0.3);
+  z-index: 3;
+`;
+
+const AutoSearchItem = styled.div`
+  padding: 10px;
+  cursor: default;  // 기본 커서로 변경하여 클릭할 수 없음을 표시
+  font-size: 14px;
+  font-weight: bold;
+  pointer-events: none;  // 클릭 이벤트를 무시
+  display: flex;
+  justify-content: space-between; // 버튼과 텍스트를 양쪽 끝에 배치
+  align-items: center;
+  button {
+    pointer-events: all;  // 버튼은 클릭할 수 있게 설정
+  }
+`;
