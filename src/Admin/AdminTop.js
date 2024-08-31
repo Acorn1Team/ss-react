3c45e8926c23125c543bb98e6406d84c47ed5ef2
hyoseonlim <hyoseonee0729@naver.com>
2024-08-31 17:56:00 +0900

    관리자

--- src/Admin/AdminTop.js
+++ src/Admin/AdminTop.js
@@ -4,7 +4,7 @@ import './Style/admin.css'
 export default function AdminTop(){
     return(
         <>
-            <h1>Scene Stealer 관리자</h1>
+            <h1><Link to="/admin" id="admin-header">Scene Stealer 관리자</Link></h1>
             <ul id="topmenu">
                 <li><Link to="/admin/fashion">패션</Link></li>
                 <li><Link to="/admin/product">상품</Link></li>
@@ -12,7 +12,6 @@ export default function AdminTop(){
                 <li><Link to="/admin/community">커뮤니티</Link></li>
                 <li><Link to="/admin/promotion">프로모션</Link></li>
                 <li><Link to="/admin/help">고객 지원</Link></li>
-                <li><Link to="/admin/statistics">통계</Link></li>
                 <li><Link to="/">사용자 페이지</Link></li>
             </ul><br/>
         </>
