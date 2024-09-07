53c229f3f64a55f3ea56702189afc64259da9edf
hyo <hyoseonee0729@naver.com>
2024-09-07 20:43:11 +0900

    관리자 상품 수정

--- src/Admin/AdminProduct/ProductManage.js
+++ src/Admin/AdminProduct/ProductManage.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
-import "./ProductManage.css"; // CSS 파일 추가
 
 export default function ProductManage() {
   const [products, setProducts] = useState([]);
@@ -14,7 +13,7 @@ export default function ProductManage() {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [currentPage, setCurrentPage] = useState(0);
-  const [pageSize, setPageSize] = useState(8);
+  const [pageSize, setPageSize] = useState(10);
   const [totalPages, setTotalPages] = useState(1);
   const [searchTriggered, setSearchTriggered] = useState(false);
   const navigate = useNavigate();
@@ -107,7 +106,6 @@ export default function ProductManage() {
     }
   }, [currentPage, pageSize, searchTriggered]);
 
-
   const handleSearch = () => {
     setCurrentPage(0);
     setSearchTriggered(true);
@@ -132,7 +130,7 @@ export default function ProductManage() {
 
   const calculateSellingPrice = (price, discountRate) => {
     if (discountRate === 0) return null;
-    return '(' + discountRate + '% 할인) ' + (price * (1 - discountRate / 100)).toFixed(0);
+    return Math.round(price * (1 - discountRate / 100));
   };
 
   const formatDate = (dateString) => {
@@ -142,9 +140,7 @@ export default function ProductManage() {
 
   const toSoldOut = async (no) => {
     try {
-      // axios.put 요청을 완료할 때까지 기다림
       await axios.put(`/admin/product/soldout/${no}`);
-      // 요청이 완료된 후 fetchProducts 호출
       fetchProducts(currentPage, pageSize);
     } catch (error) {
       console.error("Error updating product to sold out:", error);
@@ -199,192 +195,59 @@ export default function ProductManage() {
 
   return (
     <>
-      <table border={1}>
-        <thead>
-          <tr>
-            <th>번호</th>
-            <th>이름</th>
-            <th>가격</th>
-            <th>카테고리</th>
-            <th>이미지</th>
-            <th>재고</th>
-            <th>평점 / 리뷰</th>
-            <th>판매량</th>
-            <th>등록일</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.length > 0 ? (
-            products.map((item) => (
-              <>
-                <tr key={item.no}>
-                  <td>{item.no}</td>
-                  <td><span
-                      onClick={() => navigate(`/admin/product/update/${item.no}`)}
-                      style={{ cursor: "pointer", color: "blue" }}
-                    >{item.name}</span></td>
-                  <td>
-                    {item.discountRate > 0 ? (
-                    <>
-                      <span style={{ textDecoration: "line-through" }}>
-                        {item.price.toLocaleString('ko-KR')}원
-                      </span>{" "}
-                      <span style={{ color: "red", fontWeight: "bold" }}>
-                        {calculateSellingPrice(item.price, item.discountRate)}원
-                      </span>
-                    </>
-                  ) : (
-                    <>{item.price.toLocaleString('ko-KR')}원</>
-                  )}
-                  </td>
-                  <td>{item.category}</td>
-                  <td>
-                    <img
-                      src={item.pic}
-                      alt={item.name}
-                      style={{ width: "100px", height: "100px" }}
-                      />
-                  </td>
-                  <td>{item.stock}&nbsp; 
-                    <button onClick={() => toSoldOut(item.no)} disabled={item.stock === 0}>품절 처리하기</button></td>
-                  <td>
-                    <span
-                      onClick={() => toggleRowExpansion(item.no)}
-                      style={{ cursor: "pointer", color: "blue" }}
-                    >
-                      {`${item.score} (${item.reviewCount || 0}건)`}
-                    </span>
-                  </td>
-                  <td>{item.count}건</td>
-                  <td>{formatDate(item.date)}</td>
-                </tr>
-                {expandedRows.includes(item.no) && (
-                  <>
-                    <tr>
-                      <td colSpan="13">
-                        <ul>
-                          {reviews[item.no] && reviews[item.no].length > 0 ? (
-                            reviews[item.no].map((review) => (
-                              <li key={review.no}>
-                                <p>
-                                  <strong>유저 아이디: {review.userid}</strong>
-                                </p>
-                                <p>내용: {review.contents}</p>
-                                <p>평점: {review.score}</p>
-                                <p>이미지: {review.pic}</p>
-                              </li>
-                            ))
-                          ) : (
-                            <p>No reviews available</p>
-                          )}
-                        </ul>
-                      </td>
-                    </tr>
-                    {reviewPages > 1 && (
-                      <tr>
-                        <td colSpan="13" style={{ textAlign: "center" }}>
-                          <button
-                            onClick={() =>
-                              handleReviewPageChange(item.no, "prev")
-                            }
-                            disabled={currentReviewPage[item.no] === 0}
-                            >
-                            이전
-                          </button>
-                          <span style={{ margin: "0 10px" }}>
-                            {currentReviewPage[item.no] + 1} /{" "}
-                            {reviewPages[item.no]}
-                          </span>
-                          <button
-                            onClick={() =>
-                              handleReviewPageChange(item.no, "next")
-                            }
-                            disabled={
-                              currentReviewPage[item.no] + 1 >=
-                              reviewPages[item.no]
-                            }
-                            >
-                            다음
-                          </button>
-                        </td>
-                      </tr>
-                    )}
-                  </>
-                )}
-              </>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="13" style={{ textAlign: "center", padding: "20px" }}>
-              </td>
-                결과가 없습니다.
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <style>
+        {`
+          .product-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+          }
 
-      <div style={{ marginTop: "10px" }}>
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 0}
-        >
-          이전
-        </button>
-        <span style={{ margin: "0 10px" }}>
-          {currentPage + 1} / {totalPages}
-        </span>
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage + 1 === totalPages}
-        >
-          다음
+          .product-card {
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            padding: 20px;
+            margin: 10px;
+            width: 250px;
+            height: auto;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+          }
+
+          .product-card img {
+            margin: 10px 0;
+          }
+
+          .button-container {
+            text-align: center;
+            margin-top: 20px;
+          }
+
+          .button {
+            padding: 10px 20px;
+            color: black;
+            cursor: pointer;
+          }
+
+          .button:hover {
+            background-color: #45a049;
+          }
+
+          .reviews-container {
+            padding: 10px;
+            background-color: #f9f9f9;
+            border-top: 1px solid #ccc;
+          }
+        `}
+      </style>
+      <div className="button-container">
+        <button className="button" onClick={() => navigate("/admin/product/insert")}>
+          상품 추가하기
         </button>
       </div>
 
-      {/* 버튼을 중앙에 배치하기 위해 추가한 div */}
-      <div class="button-container">
-        <button
-          class="button"
-          onClick={() => navigate("/admin/product/insert")}
-        >
-          <div class="dots_border"></div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            class="sparkle"
-          >
-            <path
-              class="path"
-              stroke-linejoin="round"
-              stroke-linecap="round"
-              stroke="black"
-              fill="black"
-              d="M14.187 8.096L15 5.25L15.813 8.096C16.0231 8.83114 16.4171 9.50062 16.9577 10.0413C17.4984 10.5819 18.1679 10.9759 18.903 11.186L21.75 12L18.904 12.813C18.1689 13.0231 17.4994 13.4171 16.9587 13.9577C16.4181 14.4984 16.0241 15.1679 15.814 15.903L15 18.75L14.187 15.904C13.9769 15.1689 13.5829 14.4994 13.0423 13.9587C12.5016 13.4181 11.8321 13.0241 11.097 12.814L8.25 12L11.096 11.187C11.8311 10.9769 12.5006 10.5829 13.0413 10.0423C13.5819 9.50162 13.9759 8.83214 14.186 8.097L14.187 8.096Z"
-            ></path>
-            <path
-              class="path"
-              stroke-linejoin="round"
-              stroke-linecap="round"
-              stroke="black"
-              fill="black"
-              d="M6 14.25L5.741 15.285C5.59267 15.8785 5.28579 16.4206 4.85319 16.8532C4.42059 17.2858 3.87853 17.5927 3.285 17.741L2.25 18L3.285 18.259C3.87853 18.4073 4.42059 18.7142 4.85319 19.1468C5.28579 19.5794 5.59267 20.1215 5.741 20.715L6 21.75L6.259 20.715C6.40725 20.1216 6.71398 19.5796 7.14639 19.147C7.5788 18.7144 8.12065 18.4075 8.714 18.259L9.75 18L8.714 17.741C8.12065 17.5925 7.5788 17.2856 7.14639 16.853C6.71398 16.4204 6.40725 15.8784 6.259 15.285L6 14.25Z"
-            ></path>
-            <path
-              class="path"
-              stroke-linejoin="round"
-              stroke-linecap="round"
-              stroke="black"
-              fill="black"
-              d="M6.5 4L6.303 4.5915C6.24777 4.75718 6.15472 4.90774 6.03123 5.03123C5.90774 5.15472 5.75718 5.24777 5.5915 5.303L5 5.5L5.5915 5.697C5.75718 5.75223 5.90774 5.84528 6.03123 5.96877C6.15472 6.09226 6.24777 6.24282 6.303 6.4085L6.5 7L6.697 6.4085C6.75223 6.24282 6.84528 6.09226 6.96877 5.96877C7.09226 5.84528 7.24282 5.75223 7.4085 5.697L8 5.5L7.4085 5.303C7.24282 5.24777 7.09226 5.15472 6.96877 5.03123C6.84528 4.90774 6.75223 4.75718 6.697 4.5915L6.5 4Z"
-            ></path>
-          </svg>
-          <span class="text_button">상품 추가하기</span>
-        </button>
-      </div>
-
-      <br />
-      <br />
       <div style={{ marginBottom: "10px" }}>
         <label style={{ display: "inline-block", marginRight: "10px" }}>
           검색 :
@@ -401,14 +264,109 @@ export default function ProductManage() {
 
         {renderSearchField()}
 
-        <button
-          onClick={handleSearch}
-          style={{ padding: "5px 10px", marginRight: "10px" }}
-        >
+        <button onClick={handleSearch} style={{ padding: "5px 10px", marginRight: "10px" }}>
           검색
         </button>
         <button onClick={handleReset} style={{ padding: "5px 10px" }}>
           전체보기
+        </button>
+      </div>
+      <div className="product-container">
+        {products.length > 0 ? (
+          products.map((item) => (
+            <div key={item.no} className="product-card">
+              <div> 
+                <span
+                  onClick={() => navigate(`/admin/product/update/${item.no}`)}
+                  style={{ cursor: "pointer", color: "blue" }}
+                >
+                  {item.no}.{item.name}
+                </span>
+              </div>
+              <div>
+                <strong>평점: </strong>
+                <span
+                  onClick={() => toggleRowExpansion(item.no)}
+                  style={{ cursor: "pointer", color: "blue" }}
+                >
+                  {item.score}점 (리뷰 총 {item.reviewCount || 0}건)
+                </span>
+              </div>
+              <div>
+                {item.discountRate > 0 ? (
+                  <>
+                    <span style={{ textDecoration: "line-through" }}>
+                      {item.price.toLocaleString('ko-KR')}원
+                    </span>&nbsp;
+                    <span style={{ color: "red", fontWeight: "bold" }}>
+                      {calculateSellingPrice(item.price, item.discountRate).toLocaleString('ko-KR')}원
+                      ({item.discountRate} % 할인)
+                    </span>
+                  </>
+                ) : (
+                  <>{item.price.toLocaleString('ko-KR')}원</>
+                )}
+              </div>
+              <div>
+                <img
+                  src={item.pic}
+                  alt={item.name}
+                  style={{ width: "100px", height: "100px" }}
+                />
+              </div>
+              <div>
+                <strong>재고:</strong> {item.stock} 
+                &nbsp; 
+                <button onClick={() => toSoldOut(item.no)} disabled={item.stock === 0}>
+                  품절 처리하기
+                </button>
+              </div>
+              {expandedRows.includes(item.no) && (
+                <div className="reviews-container">
+                  <div>
+                    {reviews[item.no] && reviews[item.no].length > 0 ? (
+                      reviews[item.no].map((review) => (
+                        <div key={review.no}>
+                          <strong>{review.userid}</strong><br/>
+                          평점: {review.score}점<br/>
+                          {review.contents}<br/>
+                          <img style={{height:'50px'}} src={review.pic} alt={`${review.userid}의 후기 사진`} />
+                        <hr/></div>
+                      ))
+                    ) : (
+                      <p>No reviews available</p>
+                    )}
+                  </div>
+                </div>
+              )}
+              <div>
+                <strong>판매량:</strong> {item.count}건
+              </div>
+              <div>
+                <strong>등록일:</strong> {formatDate(item.date)}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div style={{ textAlign: "center", padding: "20px" }}>결과가 없습니다.</div>
+        )}
+      </div>
+
+      <div style={{ marginTop: "10px" }}>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 0}
+        >
+          이전
+        </button>
+        <span style={{ margin: "0 10px" }}>
+          {currentPage + 1} / {totalPages}
+        </span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage + 1 === totalPages}
+        >
+          다음
         </button>
       </div>
     </>
