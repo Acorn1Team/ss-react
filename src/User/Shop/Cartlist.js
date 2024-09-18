0dac9b297f4ad52c7374bfe80940ede4cff92a91
최지연 <ch_ooi@naver.com>
2024-09-18 16:01:28 +0900

    장바구니가 비어 있습니다 가운데 정렬 처리

--- src/User/Shop/Cartlist.js
+++ src/User/Shop/Cartlist.js
@@ -214,7 +214,7 @@ export default function CartList() {
     <>
       <h2 className={styles.cartTitle}>장바구니</h2>
       {cartItems.length === 0 ? (
-        <p>장바구니가 비어 있습니다.</p>
+        <p className={styles.cartContainer}>장바구니가 비어 있습니다.</p>
       ) : (
         <>
           <div className={styles.cartContainer}>
