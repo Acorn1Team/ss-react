76562afa17b80092626d99848d71c6ab8f5e244e
dazz6 <dazz6@kakao.com>
2024-09-22 15:12:55 +0900

    by dazz6

--- src/User/Component/FooterForm.js
+++ src/User/Component/FooterForm.js
@@ -11,6 +11,12 @@ export default function FooterForm() {
         <p>Team Daracle | 팀 다라클 | teamdaracle@gmail.com</p>
       </div>
       <p>© 2024 SceneStealer. All rights reserved.</p>
+      <div style={{ fontSize: "180%" }}>
+        본 사이트는 개인 포트폴리오를 목적으로 제작된 페이지입니다.
+        <br />
+        상업적인 목적이나 이익 창출과는 무관하며, 실제 운영되는 서비스가
+        아닙니다.
+      </div>
     </footer>
   );
 }
