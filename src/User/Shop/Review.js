3f702f565d7ad6140fad2b84ab293057c9c469c7
hyunjung <ariestail@kakao.com>
2024-10-01 17:42:10 +0900

    rose css err

--- src/User/Shop/Review.js
+++ src/User/Shop/Review.js
@@ -4,45 +4,43 @@ import { Link, useParams } from "react-router-dom";
 import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
 import "./Review.css"; // CSS 파일 추가
 
-
-
 export default function Review() {
   const { no } = useParams();
   const [reviewData, setReviewData] = useState([]);
-  const [reviewImage, setReviewImage] = useState(""); // 리뷰 이미지 상태 추가
+  const [reviewImage, setReviewImage] = useState(""); // 리뷰 이미지 상태
+  const [productImage, setProductImage] = useState(""); // 상품 이미지 상태
 
+  // 리뷰 데이터 불러오기
   const getReviewData = () => {
     axios
       .get(`/api/mypage/review/detail/${no}`)
       .then((res) => {
         setReviewData(res.data);
 
-        // 리뷰 이미지가 있는 경우 이미지 데이터를 받아오기
+        // 리뷰 이미지가 있는 경우 리뷰 이미지 사용
         if (res.data.pic) {
           setReviewImage(res.data.pic);
-        } else {
-          // 리뷰 이미지가 없으면 상품 이미지 가져오기
+        } 
+          // 리뷰 이미지가 없으면 상품 이미지 불러오기
           fetchProductImage(res.data.productNo);
-        }
+        
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
-    // 상품 이미지를 가져오는 함수 (리뷰 이미지가 없을 때 대체)
-    const fetchProductImage = (productNo) => {
-      axios
-        .get(`/api/list/product/${productNo}`)
-        .then((res) => {
-          setReviewImage(res.data.pic); // 상품 이미지 설정
-          
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
+  // 상품 이미지를 가져오는 함수
+  const fetchProductImage = (productNo) => {
+    axios
+      .get(`/api/list/product/${productNo}`)
+      .then((res) => {
+        setProductImage(res.data.pic); // 상품 이미지 설정
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     getReviewData();
@@ -66,13 +64,12 @@ export default function Review() {
   return (
     <div className="review-container">
       <div className="review-card">
-           {/* 리뷰 이미지가 없으면 상품 이미지를 대신 사용 */}
-           <img
-          src={reviewImage} // reviewImage 상태에서 가져온 이미지 사용
+        {/* 리뷰 이미지가 있을 경우 리뷰 이미지 사용, 없으면 상품 이미지 사용 */}
+        <img
+          src={reviewImage || productImage} // 리뷰 이미지가 없으면 상품 이미지 사용
           alt={`${reviewData.productName} 사진`}
           className="review-image"
         />
-        {/* <img src={reviewData.pic} alt={`${reviewData.productName} 사진`} className="review-image" /> */}
         <div className="review-content">
           <div className="review-header">
             <span className="user-nickname">@{reviewData.userNickname}</span>
@@ -80,10 +77,23 @@ export default function Review() {
           </div>
           <div className="review-text">{reviewData.contents}</div>
         </div>
+
         {/* 하단 상품 정보 섹션에 링크 추가 */}
         <Link to={`/user/shop/productlist/detail/${reviewData.productNo}`} className="product-info-link">
           <div className="product-info">
-            {/* <img src={reviewData.productImage} alt={`${reviewData.productName}`} className="product-thumbnail" /> */}
+           {/* 제품 정보에서 항상 제품 이미지 출력 */}
+           {productImage && (
+              <img
+                src={productImage} // 제품 이미지 URL 사용
+                alt={`${reviewData.productName} 사진`}
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  objectFit: "cover",
+                }}
+              />
+            )}
             <div className="product-details">
               <span>제품정보</span>
               <span className="product-name">{reviewData.productName}</span>
